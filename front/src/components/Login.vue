0b94ff15f68286f2c6b642b6bb7135261a90408b
takuyaFujimoto <takuya.fujimoto@i-vinci.co.jp>
2019-05-19 13:39:55 +0900

    front側
    UI用フレームワーク追加
    ディレクトリ構成の整理

---
+++ front/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-content">
+    <form class="login-form">
+      <v-text-field
+        label="Name">
+      </v-text-field>
+      <v-text-field
+        label="password"
+        type="password"
+        >
+      </v-text-field>
+      <v-btn
+        class="white--text"
+        block=ture
+        @click="submit"
+        color="#7fbfff">
+          ログイン
+      </v-btn>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {},
+}
+</script>
+<style scoped>
+.login-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: 50%;
+  min-width: 600px;
+  height: 400px;
+  border: solid 1px #c7c7c7;
+}
+
+.login-form {
+  margin-top: 60px;
+  padding: 50px 250px;
+}
+
+</style>
---
+++ front/src/container/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <HelloWorld :hoge="hoge"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    hoge: 'hoge',
+    piyo: 'piyo',
+    fuga: 'fuga'
+  })
+}
+</script>
